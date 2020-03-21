a8e18c046c0f0c6be579885eb02b985101be54ff
Daniel Kronovet <kronovet@gmail.com>
2020-03-21 15:18:30 -0700

    Set up minimal Bolt server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
