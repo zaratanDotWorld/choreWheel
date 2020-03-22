4f07fef4c0a99f2a3bbbe79c3785ca59d24e51a4
Daniel Kronovet <kronovet@gmail.com>
2020-03-22 14:28:48 -0700

    Create first round-trip bot flow

--- src/app.js
+++ src/app.js
@@ -2,15 +2,49 @@ require('dotenv').config()
 
 const { App } = require('@slack/bolt');
 
-// Initializes your app with your bot token and signing secret
+// Create the app
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
+// Define the interface
 
+app.message('hello', ({ message, say }) => {
+  // say(`Hey there <@${message.user}>!`);
+  say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ]
+  });
+});
+
+app.action('button_click', ({ body, ack, say }) => {
+  ack();
+
+  say(`<@${body.user.id}> clicked the button`);
+});
+
+// Launch the app
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
 })();
+
+// Fin
