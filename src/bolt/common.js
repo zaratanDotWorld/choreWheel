1f585c73ecdc2c48fa6d3bac77d36aca84092e3e
Daniel Kronovet <kronovet@gmail.com>
2023-01-08 20:43:08 -0500

    Use escaped channel names when setting channels

--- src/bolt/common.js
+++ src/bolt/common.js
@@ -75,20 +75,25 @@ exports.addReaction = async function (app, oauth, payload, emoji) {
 };
 
 exports.setChannel = async function (app, oauth, channelType, command) {
-  const channelName = command.text;
-  const houseId = command.team_id;
-
   const userInfo = await exports.getUser(app, oauth, command.user_id);
   if (userInfo.user.is_admin) {
-    // TODO: return a friendly error if the channel doesn't exist
-    const res = await app.client.conversations.list({ token: oauth.bot.token });
-    const channelId = res.channels.filter(channel => channel.name === channelName)[0].id;
-    await Admin.updateHouse({ slackId: houseId, [channelType]: channelId });
+    const regex = /<#(\w+)|([a-zA-Z0-9_-]+)>/g; // Matches`<#CHANNELID|channel-name>`
+    const channelInfo = [ ...command.text.matchAll(regex) ];
 
-    const text = `${channelType} set to ${channelName} :fire:\n` +
-      'Please add the bot to the channel';
-    await exports.postEphemeral(app, oauth, command, text);
-    console.log(`Set ${channelType} to ${channelName}`);
+    if (channelInfo.length === 2) {
+      const houseId = command.team_id;
+      const channelId = channelInfo[0][1];
+      const channelName = channelInfo[1][2];
+
+      await Admin.updateHouse({ slackId: houseId, [channelType]: channelId });
+
+      const text = `${channelType} set to *${channelName}* :fire:\n` +
+        'Please add the bot to the channel';
+      await exports.postEphemeral(app, oauth, command, text);
+    } else {
+      const text = `*${command.text}* is not a valid channel...`;
+      await exports.postEphemeral(app, oauth, command, text);
+    }
   } else {
     const text = 'Only admins can set the channels...';
     await exports.postEphemeral(app, oauth, command, text);
