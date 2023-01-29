6a5c6e33fb2b52d552752a1e95a94704656e4fd8
Daniel Kronovet <kronovet@gmail.com>
2023-01-29 15:47:39 -0500

    Remove extraneous config options

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: process.env.PG_CONNECTION_DEV
-  },
   test: {
     client: 'pg',
     connection: process.env.PG_CONNECTION_TEST
-  },
-  production: {
-    client: 'pg',
-    connection: process.env.PG_CONNECTION_PROD
   }
 };
