46e1e5e772e5684bfb1122f00427ea302a56a045
Daniel Kronovet <kronovet@gmail.com>
2023-02-26 13:12:04 -0800

    Update dampingFactor to 0.95

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ exports.penaltyDelay = exports.choresPollLength + 6 * HOUR; // TODO: make robust
 exports.penaltyIncrement = 10; // Per half heart
 exports.achievementBase = 20;
 exports.implicitPref = 0.25;
-exports.dampingFactor = 0.9;
+exports.dampingFactor = 0.95;
 
 // Hearts
 exports.heartsPollLength = 3 * DAY;
