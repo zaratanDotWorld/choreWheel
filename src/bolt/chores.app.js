c893025971db7c91ab6661a4309a4fb711d3fa78
Daniel Kronovet <kronovet@gmail.com>
2023-08-04 08:47:38 +0300

    Fix bug to send users message when losing hearts

--- src/bolt/chores.app.js
+++ src/bolt/chores.app.js
@@ -69,7 +69,7 @@ app.event('app_home_opened', async ({ body, event }) => {
 
     // Give monthly penalty if needed
     const [ penaltyHeart ] = await Chores.addChorePenalty(houseId, residentId, now);
-    if (penaltyHeart !== undefined && penaltyHeart.value > 0) {
+    if (penaltyHeart !== undefined && penaltyHeart.value < 0) {
       const { choresChannel } = await Admin.getHouse(houseId);
       const text = `You missed too many chores last month, and lost *${penaltyHeart.value.toFixed(1)}* hearts...`;
       await common.postEphemeral(app, choresOauth, choresChannel, residentId, text); // TODO: make public?
