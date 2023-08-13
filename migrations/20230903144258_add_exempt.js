ad68417194fd4575ec9e3f927db7e92fe016a399
Daniel Kronovet <kronovet@gmail.com>
2023-08-13 11:36:49 -0400

    Add exemptAt to Resident table

---
+++ migrations/20230903144258_add_exempt.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('Resident', function(t) {
+    t.timestamp('exemptAt');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('Resident', function(t) {
+    t.dropColumn('exemptAt');
+  });
+};
