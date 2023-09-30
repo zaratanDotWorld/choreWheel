da513d96a3f690b7bc6a9dfaf07cc66e71a6f858
Daniel Kronovet <kronovet@gmail.com>
2023-09-30 09:56:52 -0400

    Create ThingProposal

---
+++ migrations/20230930000620_ThingProposal.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('ThingProposal', function(t) {
+    t.increments('id').unsigned().primary();
+    t.timestamps(useTimestamps = true, defaultToNow = true, useCamelCase = true);
+    t.string('houseId').references('House.slackId').notNull();
+    t.string('proposedBy').references('Resident.slackId').notNull();
+    t.integer('thingId').references('Thing.id');
+    t.string('type').notNull();
+    t.string('name').notNull();
+    t.float('value').notNull().defaultTo(0);
+    t.boolean('active').notNull().defaultTo(true);
+    t.json('metadata').notNull().defaultTo({});
+    t.integer('pollId').references('Poll.id').notNull();
+    t.timestamp('resolvedAt');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('ThingProposal');
+};
