d1e14b19e030cf648cafca964b71474ab545130d
Daniel Kronovet <kronovet@gmail.com>
2023-10-06 17:08:56 -0400

    Add minVotes to Poll schema

---
+++ migrations/20231006130926_poll_minvote.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('Poll', function(t) {
+    t.integer('minVotes');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('Poll', function(t) {
+    t.dropColumn('minVotes');
+  });
+};
