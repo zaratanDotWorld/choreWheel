add440e20683c23fad20b1dc3e60edba260b74e2
Daniel Kronovet <kronovet@gmail.com>
2023-10-08 10:24:05 -0400

    Clean up after #90

--- migrations/20200329100300_Poll.js
+++ migrations/20200329100300_Poll.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('Poll', function(t) {
         t.increments('id').unsigned().primary();
         t.timestamps(useTimestamps = true, defaultToNow = true, useCamelCase = true);
-        // t.string('houseId').references('House.slackId').notNull();
+        t.string('houseId').references('House.slackId').notNull();
         t.timestamp('startTime').notNull();
         t.timestamp('endTime').notNull();
         t.integer('minVotes').notNull();
