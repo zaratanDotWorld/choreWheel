94def8958fc1361e91e54bffa5d2e7a5a36ee244
Daniel Kronovet <kronovet@gmail.com>
2023-10-25 17:16:37 -0400

    Add metadata to House schema

---
+++ migrations/20231025141958_house_metadata.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('House', function(t) {
+    t.json('metadata').notNull().defaultTo({});
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('House', function(t) {
+    t.dropColumn('metadata');
+  });
+};
