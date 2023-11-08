37a3f5fc940b5a8be9f7ea8100e345a4ee33a926
Daniel Kronovet <kronovet@gmail.com>
2023-11-08 18:06:00 -0500

    Use try/catch to handle scoping issues

--- src/bolt/common.js
+++ src/bolt/common.js
@@ -243,8 +243,15 @@ exports.updateVoteResults = async function (app, oauth, pollId) {
   const { metadata } = await Polls.getPoll(pollId);
   const valid = await Polls.isPollValid(pollId);
 
-  const body = await exports.getMessage(app, oauth, metadata.channel, metadata.ts);
-  const message = body.messages[0];
+  // TODO: Remove this try/catch once all workspaces are upgraded
+  let message;
+  try {
+    const body = await exports.getMessage(app, oauth, metadata.channel, metadata.ts);
+    message = body.messages[0];
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 
   // Parse current vote counts;
   const voteBlock = message.blocks.length - 1;
