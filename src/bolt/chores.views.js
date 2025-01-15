14082efa01ac3d43b207540e9c74c99777e7d34e
Daniel Kronovet <kronovet@gmail.com>
2025-01-14 19:22:37 -0500

    Add /chores-activate command

--- src/bolt/chores.views.js
+++ src/bolt/chores.views.js
@@ -147,6 +147,63 @@ exports.choresStatsView = function (choreClaims, choreBreaks, choreStats) {
   };
 };
 
+exports.choresActivateView = function (residents) {
+  const header = 'Update activation status';
+  const mainText = 'Activated residents *owe chores*, and can *create or vote on polls*.\n\n' +
+    'Choose some residents to update. ' +
+    'You can update *all* residents in the workspace, or only *a few*.';
+  const residentsText = `*Currently active residents* (${residents.length}): ` +
+    residents.slice(0, 100).map(r => `<@${r.slackId}>`).join(', ');
+
+  const options = [
+    { value: 'true', text: common.blockMarkdown('*Activate* some residents') },
+    { value: 'false', text: common.blockMarkdown('*Deactivate* some residents') },
+  ];
+
+  const blocks = [];
+  blocks.push(common.blockHeader(header));
+  blocks.push(common.blockSection(mainText));
+  blocks.push(common.blockSection(residentsText));
+  blocks.push(common.blockDivider());
+  blocks.push(common.blockInput(
+    'Update action',
+    {
+      type: 'radio_buttons',
+      action_id: 'action',
+      initial_option: options[0],
+      options,
+    },
+  ));
+  blocks.push(common.blockInput(
+    'Update ~all~ residents',
+    {
+      type: 'checkboxes',
+      action_id: 'select_all',
+      options: [ { value: 'true', text: common.blockPlaintext('Yes') } ],
+    },
+    true,
+  ));
+  blocks.push(common.blockInput(
+    'Update ~selected~ residents',
+    {
+      action_id: 'residents',
+      type: 'multi_conversations_select',
+      filter: common.userFilter,
+      placeholder: common.blockPlaintext('Choose some residents'),
+    },
+    true,
+  ));
+
+  return {
+    type: 'modal',
+    callback_id: 'chores-activate-callback',
+    title: TITLE,
+    close: common.CLOSE,
+    submit: common.SUBMIT,
+    blocks,
+  };
+};
+
 exports.choresResetView = function () {
   const header = 'Reset chore points';
   const mainText = 'Reset chore points for the workspace. ' +
