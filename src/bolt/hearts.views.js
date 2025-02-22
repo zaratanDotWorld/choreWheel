871da911770280682def9a4c5563e1f1ab625bf1
Daniel Kronovet <kronovet@gmail.com>
2025-02-22 17:37:20 -0500

    Add /hearts-reset command

--- src/bolt/hearts.views.js
+++ src/bolt/hearts.views.js
@@ -83,6 +83,28 @@ exports.heartsHomeView = function (heartsChannel, isActive, numHearts, maxHearts
   };
 };
 
+// Slash commands
+
+exports.heartsResetView = function () {
+  const header = 'Reset hearts';
+  const mainText = 'Reset hearts for the workspace. ' +
+  'All hearts will be reset to 5.\n\n' +
+  ':warning: *This action cannot be undone!* :warning:';
+
+  const blocks = [];
+  blocks.push(common.blockHeader(header));
+  blocks.push(common.blockSection(mainText));
+
+  return {
+    type: 'modal',
+    callback_id: 'hearts-reset-callback',
+    title: TITLE,
+    close: common.CLOSE,
+    submit: common.SUBMIT,
+    blocks,
+  };
+};
+
 // Challenge flow
 
 exports.heartsChallengeView = function (numResidents) {
