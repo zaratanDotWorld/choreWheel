8c188a7215e0bda5b7a57f7cd6b6f4b70e432442
Daniel Kronovet <kronovet@gmail.com>
2025-03-08 09:03:58 +0200

    Remove chore description from claim post body

--- src/bolt/chores.views.js
+++ src/bolt/chores.views.js
@@ -313,7 +313,7 @@ exports.getSparkles = function (monthlyPoints) {
   return ':sparkles:'.repeat(Math.max(numSparkles, 0)); // Handle negative points
 };
 
-exports.choresClaimCallbackView = function (claim, name, description, minVotes, achivementPoints, monthlyPoints) {
+exports.choresClaimCallbackView = function (claim, name, minVotes, achivementPoints, monthlyPoints) {
   const achievement = exports.getAchievement(achivementPoints);
   const sparkles = exports.getSparkles(monthlyPoints);
 
@@ -322,9 +322,6 @@ exports.choresClaimCallbackView = function (claim, name, description, minVotes, 
 
   const blocks = [];
   blocks.push(common.blockSection(mainText));
-  if (description) {
-    blocks.push(common.blockSection(description));
-  }
   blocks.push(common.blockSection(common.makeVoteText(minVotes, choresPollLength)));
   blocks.push(common.blockActions(common.makeVoteButtons(claim.pollId, 1, 0)));
   return blocks;
